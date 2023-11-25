1f90229e9dd3018e61a42d3947e8338f34da0949
brutchley <bhutchison0901@yahoo.com>
2023-11-25 17:17:31 -0600

    created one class for a squre shape so far, need to test this to make sure it works.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -3,3 +3,5 @@ const sum = require('../lib/shapes')
 test('Accurately produces a sum of two entered numbers', () => {
     expect(sum(1, 2)).toBe(3)
 });
+
+//verified Jest is correctly installed by utilizing simple sum function, and test passes on my device.
