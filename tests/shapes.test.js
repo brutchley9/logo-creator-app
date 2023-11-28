6a447f550182a810e5c8daa898a6d5709e3887ad
brutchley <bhutchison0901@yahoo.com>
2023-11-28 14:54:20 -0600

    FINALLY got a working testing suite with Jest for all three of my shape classes. This was done by delcaring 'shapecolor = green' globally and then running this value through all three tests at the same time, which gave me three passes

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -14,11 +14,31 @@ const {Square, Triangle, Circle} = require('../lib/shapes')
 
 //verified Jest is correctly installed by utilizing simple sum function, and test passes on my device.
 
+let shapecolor = "green"
+
 describe('Square', () => {
     it ("should render a string of code corresponding to the square shape", () => {
         square = new Square();
-        const shapecolor = "green"
-        const result = square.render(`<rect x="10" y="10" width="100" height="100" fill="${shapecolor}"/>`)
-        expect(result).toEqual('<rect x="10" y="10" width="100" height="100" fill="green"/>')
+        //let shapecolor = "green"
+        let result = square.render(`<rect x="10" y="10" width="100" height="100" fill="${shapecolor}"/>`)
+        expect(result).toEqual(`<rect x="10" y="10" width="100" height="100" fill="green"/>`)
+    })
+});
+
+describe('Triangle', () => {
+    it ("should render a string of code corresponding to the triangle shape", () => {
+        triangle = new Triangle();
+        //let shapecolor = "red"
+        let result = triangle.render(`<polygon points="10,-10 60,150 110,-10" fill="${shapecolor}"/>`)
+        expect(result).toEqual(`<polygon points="10,-10 60,150 110,-10" fill="green"/>`)
+    })
+});
+
+describe('Circle', () => {
+    it ("should render a string of code corresponding to the circle shape", () => {
+        circle = new Circle();
+        //let shapecolor = "blue"
+        let result = circle.render(`<circle cx="60" cy="60" r="60" fill="${shapecolor}"/>`)
+        expect(result).toEqual(`<circle cx="60" cy="60" r="60" fill="green"/>`)
     })
 });
