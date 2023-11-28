edf43b7c3fe8d4dd0d8cf61b8c68538fe202d6b4
brutchley <bhutchison0901@yahoo.com>
2023-11-28 11:50:42 -0600

    still working with jest testing syntax but doesnt work yet

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,4 +1,4 @@
-const sum = require('../lib/shapes')
+/*const sum = require('../lib/shapes')*/
 
 const {Square, Triangle, Circle} = ('../lib/shapes')
 
@@ -8,13 +8,17 @@ const {Square, Triangle, Circle} = ('../lib/shapes')
 
 
 
-test('Accurately produces a sum of two entered numbers', () => {
+/*test('Accurately produces a sum of two entered numbers', () => {
     expect(sum(1, 2)).toBe(3)
-});
+});*/
 
 //verified Jest is correctly installed by utilizing simple sum function, and test passes on my device.
 
-test('Accurately renders a square', () => {
-    const renderSquare = render(<rect x="10" y="10" width="100" height="100" fill="${this.shapecolor}"/>);
-    expect(renderSquare(Square)).toBeInTheDocument();
+describe('Square', () => {
+    it ("should render a string of code corresponding to the square shape", () => {
+        square = new Square;
+        const shapecolor = "green"
+        const result = square.isEqual(`<rect x="10" y="10" width="100" height="100" fill="${shapecolor}"/>`)
+        expect(result).toEqual('<rect x="10" y="10" width="100" height="100" fill="green"/>')
+    })
 });
